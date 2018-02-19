a4acf5d99b76e9e9e08d6d943066b16a52d73ad1
zhoujie <455149836@qq.com>
2018-02-20 00:43:01 +0800

    blog update

--- my_blog/my_blog/urls.py
+++ my_blog/my_blog/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from article import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home),
+    url(r'^(?P<my_args>\d+)/$', views.detail, name='detail'),
 ]
