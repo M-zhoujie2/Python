ff3eed992843d724ea9396e766d08d65e5589c58
zhoujie <455149836@qq.com>
2018-02-20 23:29:03 +0800

    update blog code

--- my_blog/gunicorn.conf.py
+++ my_blog/gunicorn.conf.py
@@ -3,7 +3,7 @@
 
 import multiprocessing
 
-bind = "127.0.0.1:8888"
+bind = "127.0.0.1:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "gevent"
 proc_name = "mdwiki"
@@ -12,8 +12,8 @@ chdir = "/root/api/python/my_blog"
 # daemon = False
 # group = "root"
 loglevel = "info"
-errorlog = chdir + "/log/gunicorn/error.log"
-accesslog = chdir + "/log/gunicorn/access.log"
+errorlog = chdir + "/logs/gunicorn/error.log"
+accesslog = chdir + "/logs/gunicorn/access.log"
 # ssl
 # keyfile=
 # certfile=
