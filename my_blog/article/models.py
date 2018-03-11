0f719ba2b45bec6470179ddc0836fe2dfd78603d
zhoujie <455149836@qq.com>
2018-03-11 23:08:44 +0800

    update blog code

--- my_blog/article/models.py
+++ my_blog/article/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 # Create your models here.
@@ -10,6 +11,10 @@ class Article(models.Model):
     category = models.CharField(max_length=50, blank=True)  # 博客标签
     date_time = models.DateTimeField(auto_now_add=True)  # 博客日期
     content = models.TextField(blank=True, null=True)  # 博客文章正文
+
+    def get_absolute_url(self):
+        path = reverse('detail', kwargs={'id':self.id})
+        return "http://127.0.0.1:8000%s" % path
 
     # python2使用__unicode__, python3使用__str__
     def __unicode__(self):
--- my_blog/my_blog/urls.py
+++ my_blog/my_blog/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from article import views
+from article.views import RSSFeed
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -23,5 +24,7 @@ urlpatterns = [
     url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
     url(r'^archives/$', views.archives, name='archives'),
     url(r'^aboutme/$', views.about_me, name='about_me'),
-    url(r'^tag(?P<tag>\w+)/$', views.search_tag, name='search_tag')
+    url(r'^tag(?P<tag>\w+)/$', views.search_tag, name='search_tag'),
+    url(r'^search/$', views.blog_search, name='search'),
+    url(r'^feed/$', RSSFeed(), name='RSS')
 ]
